69faaf2299948a12cc75f986b0f70d891bffdb0f
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-08-18 01:23:55 +0300

    feat: add new tempalates and new controller

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    product_list = Product.objects.all
+    context = {"products": product_list}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk) #Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, "products_detail.html", context)
+
+
