753a21e4183452afe9a49e6dad092814a502b1b2
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-10-06 00:26:47 +0300

    feat: add lint

--- catalog/views.py
+++ catalog/views.py
@@ -6,18 +6,18 @@ from catalog.forms import ProductForm
 from catalog.models import Product
 
 
-
-
 class CatalogListView(ListView):
     model = Product
     form_class = ProductForm
 
+
 # app_name/<model_name>_<action>
-#прошлая реализация
-#def home(request):
+# прошлая реализация
+# def home(request):
 #     product_list = Product.objects.all
 #     context = {"products": product_list}
 #     return render(request, "home.html", context)
+
 
 class ContactTemplateView(TemplateView):
     template_name = "catalog/contacts.html"
@@ -26,7 +26,8 @@ class ContactTemplateView(TemplateView):
 class CatalogDetailView(DetailView):
     model = Product
 
-#прошлая реализация
+
+# прошлая реализация
 # def products_detail(request, pk):
 #     product = get_object_or_404(Product, pk=pk) #Product.objects.get(pk=pk)
 #     context = {"product": product}
@@ -36,19 +37,18 @@ class CatalogDetailView(DetailView):
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    template_name = 'catalog/product_form.html'
-    success_url =  reverse_lazy('catalog:product_list')
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    template_name = 'catalog/product_form.html'
-    success_url =  reverse_lazy('catalog:product_list')
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    template_name = 'catalog/product_confirm_delete.html'
-    success_url =  reverse_lazy('catalog:product_list')
-
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:product_list")
