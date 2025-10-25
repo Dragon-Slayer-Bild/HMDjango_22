1e055fa2b1217595a502eebdb87d0b5669a7bbac
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-10-25 18:28:13 +0300

    feat: add migtations and add LoginRequiredMixin

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 
+
 from catalog.models import Product
 
 
--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/product_list/"
-
+LOGIN_URL = "users:login"
 
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465
